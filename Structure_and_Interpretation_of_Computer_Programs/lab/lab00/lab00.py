ac5aba28f5d1c4b94b8801dfd5fba4644a1b0ee5
pepperRabbit <wuxt18@outlook.com>
2020-09-24 21:26:02 +0800

    some update

---
+++ Structure_and_Interpretation_of_Computer_Programs/lab/lab00/lab00.py
@@ -0,0 +1,26 @@
+def twenty_twenty():
+    """Come up with the most creative expression that evaluates to 2020,
+    using only numbers and the +, *, and - operators.
+
+    >>> twenty_twenty()
+    2020
+    """
+    return 2019 + 1
+
+def sum(a, b):
+    """Compute the sum of a and b
+    
+    >>> sum(1, 2)
+    3
+    >>> sum(3, 8)
+    11
+    """
+    return a + b
+
+def square_sum(a, b):
+    """Compute the sum of square a and square b
+
+    >>> square_sum(3, 4)
+    25
+    """
+    return a ** 2 + b ** 2
